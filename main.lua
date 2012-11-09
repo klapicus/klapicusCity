bc9c575fc9afd0eb1cdd2ae6b480b6f906240105
kyle caunt <kylecaunt@me.com>
2012-11-09 16:31:24 +1100

    Grid draws city tiles
    - based on values form vars including
    +local gridSizeWidth = 19
    +local gridSizeHeight = 19
    +local gridSpacingX = 20
    +local gridSpacingY = 20
    
    plus.
    
    now stores new grids in a table olong with reral world co-ords

--- main.lua
+++ main.lua
@@ -6,13 +6,23 @@
 
 -- Vars
 
-local regionWidth = 37
-local regionHeight = 49
+local cityWidth = 10
+local cityHeight = 10
+local gridSizeWidth = 19
+local gridSizeHeight = 19
+local gridSpacingX = 20
+local gridSpacingY = 20
+local cityGrids = {}
 
-for x=1, regionWidth do
-     for y=1, regionHeight do
-         local square = display.newRect(x * 20,y * 20,10,10)
+for x=1, cityWidth do
+    cityGrids[x] = {}
+     for y=1, cityHeight do
+         cityGrids[x][y] = {}
+         cityGrids[x][y].gridObj = display.newRect(x * gridSpacingX, y* gridSpacingY,gridSizeWidth,gridSizeHeight)
+         cityGrids[x][y].realWorld = {x = x * gridSpacingX, y = y* gridSpacingY}
 
      end
 end
+
+
 
